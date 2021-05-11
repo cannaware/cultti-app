52b74fc28c9b5272e2cbff6c716506278cd8b2d4
cannawaredev <cannaware.io@gmail.com>
2021-05-11 20:48:17 -0300

    force https from client side

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,16 @@ import Layout from '../components/Layout';
 import 'tailwindcss/tailwind.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
+  // Check the URL starts with 'http://xxxxx' protocol, if it does then redirect to 'https://xxxxx' url of same resource
+  if (typeof window !== 'undefined') {
+    const httpTokens: string[] | null = /^http:\/\/(.*)$/.exec(window.location.href);
+    const isDev = window.location.hostname === 'localhost';
+
+    if (httpTokens && !isDev) {
+      window.location.replace('https://' + httpTokens[1]);
+    }
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
