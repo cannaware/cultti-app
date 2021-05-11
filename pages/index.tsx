eb3c0f3e6f0217d45d5c3409c75c2049c5a7c756
cannawaredev <cannaware.io@gmail.com>
2021-05-11 01:42:30 -0300

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,41 @@
 import { NextPage } from 'next';
-import { useState } from 'react';
+// import { useState } from 'react';
 import Link from 'next/link';
 
-interface Launch {
-  flight_number: number;
-  details: string;
-  launch_year: string;
-}
+// interface Launch {
+//   flight_number: number;
+//   details: string;
+//   launch_year: string;
+// }
 
-interface Props {
-  launch: Launch;
-}
+// interface Props {
+//   launch: Launch;
+// }
 
-const IndexPage: NextPage<Props> = ({ launch }) => {
-  const [nextLaunch, setNextLaunch] = useState(launch);
+const IndexPage: NextPage = () => {
+  // const [nextLaunch, setNextLaunch] = useState(launch);
 
-  const getNextLaunch = async (): Promise<Launch> => {
-    const response = await fetch('https://api.spacexdata.com/v3/launches/next');
-    const newLaunch = await response.json();
-    setNextLaunch(newLaunch);
-    return newLaunch;
-  };
+  // const getNextLaunch = async (): Promise<Launch> => {
+  //   const response = await fetch('https://api.spacexdata.com/v3/launches/next');
+  //   const newLaunch = await response.json();
+  //   setNextLaunch(newLaunch);
+  //   return newLaunch;
+  // };
 
-  if (!nextLaunch?.flight_number) {
-    getNextLaunch();
-    return null;
-  }
+  // if (!nextLaunch?.flight_number) {
+  //   getNextLaunch();
+  //   return null;
+  // }
 
   return (
     <main className="text-black dark:text-white">
       <div className="mb-3">
         <h1 className="text-3xl font-bold">¡Hola!</h1>
         <p className="text-gray-500 dark:text-green-500 text-sm font-bold uppercase">
-          Next SpaceX Launch: {nextLaunch.flight_number}
+          Comencemos a cultivar
         </p>
       </div>
       <div className="m-2">
-        <p>{nextLaunch.details}</p>
-        <p>{nextLaunch.launch_year}</p>
         <p>
           <Link href="/setup/paso-1">
             <a>Setup</a>
