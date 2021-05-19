fabfdcc88abeee21cf11dd5756d1bc66ee759f0a
cannawaredev <cannaware.io@gmail.com>
2021-05-19 01:50:41 -0300

    improve Google Fonts loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
 import Layout from '@components/Layout';
 import SessionContext from '@contexts/SessionContext';
